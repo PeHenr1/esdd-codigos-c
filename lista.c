96e221d259314a19b32b980a5b20cdcf40250c83
PeHenr1 <phenriqueaissa@gmail.com>
2024-10-11 14:53:38 -0300

    lista quase completo

--- lista.c
+++ lista.c
@@ -16,9 +16,20 @@ t_list* create_list(int max) {
     return list;
 }
 
+void print_list(t_list* list) {
+    for (int i = 0; i < list->n; i++) {
+        printf("%d ", list->items[i]);
+    }
+    printf("\n");
+}
+
 void destroy_list(t_list* list) {
     free(list->items);
     free(list);
+}
+
+int size(t_list* list){
+    return list->n;
 }
 
 int is_full(t_list* list){
@@ -34,9 +45,99 @@ int append(t_list* list, int e){
     return 0;
 }
 
-int insert(){}
+int insert(t_list* list, int e, int index){
+    if(index >= list->n) return 0;
+    if(!is_full(list)){
+        list->n++;
+        for (int i = size(list); i > index ; i--){
+            list->items[i] = list->items[i - 1];
+        }
+        list->items[index] = e;
+        return 1;
+    }
+    return 0;
+}
 
-//int update(){}
+int update(t_list* list, int e, int index){
+    if(index >= list->n) return 0;
+    if(!is_full(list)){
+        for (int i = 0; i < size(list) ; i++){
+            if(i == index){
+                list->items[index] = e;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 
+int removeByIndex(t_list* list, int index){
+    if(index >= list->n || index >= list->max) return 0;
+    for (int i = 0; i < size(list); i++){
+        if(i == index){
+            for(int j = i; j < size(list)-1; j++){
+                list->items[j] = list->items[j+1];
+            }
+            list->n--;
+            return 1;
+        }
+    }
+    return 0;
+}
 
+int removeByElement(t_list* list, int e){
+    for (int i = 0; i < size(list); i++){
+        if(list->items[i] == e){
+            for(int j = i; j < size(list)-1; j++){
+                list->items[j] = list->items[j+1];
+            }
+            list->n--;
+            return 1;
+        }
+    }
+    return 0;
+}
 
+int index_of(t_list* list, int e){
+    for (int i = 0; i < size(list); i++){
+        if(list->items[i] == e){
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    t_list* list;
+
+    list = create_list(10);
+    append(list, 5);
+    append(list, 10);
+    append(list, 8);
+    append(list, 5);
+
+    print_list(list);
+
+    printf("%d\n",index_of(list,5));
+
+    insert(list,1,0);
+    print_list(list);
+
+    insert(list,222,5); 
+    print_list(list);   // print igual ao de cima pq nao rola, indice dado é invalido
+
+    insert(list,666,3);
+    print_list(list);
+
+    update(list,0,3);
+    print_list(list);
+
+    removeByIndex(list,3);
+    print_list(list);
+
+    removeByElement(list,5);
+    print_list(list);
+
+    destroy_list(list);
+    return 0;
+}
