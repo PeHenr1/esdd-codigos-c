68360d56baec856827dad06e77854b8dfbf0963a
PeHenr1 <phenriqueaissa@gmail.com>
2024-10-24 10:37:27 -0300

    update arvore.c

--- arvore.c
+++ arvore.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct treeNode{
+	int item;
+	struct treeNode *left;
+	struct treeNode *right;
+}
+
 int main(){
 	printf("Esto es un ejemplo de codigo hecho por el terminal.");
 	return 1;
